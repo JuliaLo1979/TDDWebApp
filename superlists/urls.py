0121081f55800b754b2100c876ff0eaeaea86114
Julia Lo <chiahui.lo@gmail.com>
2019-01-03 15:51:53 +0800

    Change project urls.py, lists/test.py, lists/views.py.

--- superlists/urls.py
+++ superlists/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+#在django 2.0後,URL的對應改用path(),此例中是Function views.
+#先import app的views, 再增加一條path
 from django.contrib import admin
 from django.urls import path
+from lists import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+#    path('admin/', admin.site.urls),
+    path('', views.home_page, name='home')
 ]
